8704a991ee10a631663bfbd835b5419b83027ecc
SW <SW@SW-PC2>
2018-09-03 10:13:43 -0300

    Se agrego el WebSite

---
+++ MCGA.UAI.WebSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MCGA.UAI.WebSite.Startup))]
+namespace MCGA.UAI.WebSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
